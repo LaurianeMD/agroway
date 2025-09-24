012b913cd669a94973c5f385898a72313a6e58be
Lauriane <dmbagdjelauriane@gmail.com>
2025-09-24 15:39:14 +0000

    update files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/agroway/" : "/",   // 👈 IMPORTANT
+  build: {
+    sourcemap: true, // 
+  },
   server: {
     host: "::",
     port: 8080,
